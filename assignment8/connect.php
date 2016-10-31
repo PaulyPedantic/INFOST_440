ab75da4c78e9c881e513f96b9b40b0b59a91e803
Pauly Russ <paulydanyl@gmail.com>
2016-10-31 18:07:53 -0500

    upload javascript and connection file

---
+++ assignment8/connect.php
@@ -0,0 +1,11 @@
+<?php
+DEFINE ('DB_HOST', 'localhost'); //Database server -- Typically "localhost"
+DEFINE ('DB_USER', 'paulruss_assign8'); //Database User Name
+DEFINE ('DB_PASSWORD', 'DXMgX?7,NA5{;oge{_'); //Database User Password
+DEFINE ('DB_NAME', 'paulruss_assignment8'); //Database Name
+
+
+//This connects us to the database schul253_week9ex
+$db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ('Could not connect to SOIS MySQL server with error: ' . mysqli_connect_error());
+
+?>
